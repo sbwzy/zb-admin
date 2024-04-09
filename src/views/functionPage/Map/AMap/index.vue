f66b8444e725905c44cdb6ffe61c137cdec33fce
lishujian <591926881@qq.com>
2024-04-09 21:37:27 +0800

    高德地图

--- src/views/functionPage/Map/AMap/index.vue
+++ src/views/functionPage/Map/AMap/index.vue
@@ -1,77 +1,52 @@
-<template>
-  <div id="map" style="height: 600px"></div>
-</template>
+<script setup>
+  import { onMounted, onUnmounted } from 'vue'
+  import AMapLoader from '@amap/amap-jsapi-loader'
 
-<script lang="ts" setup name="ecnuGisMap">
-  import { onMounted } from 'vue'
-  import L from 'leaflet'
-  import 'leaflet/dist/leaflet.css'
-
-  const initMap = () => {
-    const map = L.map('map').setView([31.24534249284388, 121.27915601730348], 14)
-
-    const bounds = map.getBounds()
-    const ne = bounds.getNorthEast()
-    const sw = bounds.getSouthWest()
-    const bbox = `${sw.lat},${sw.lng},${ne.lat},${ne.lng}`
-
-    const wmsLayer = L.tileLayer
-      .wms('http://webgis.ecnu.edu.cn/qgis/qgis_mapserv.fcgi.exe', {
-        service: 'WMS',
-        map: '../projects/youli/youli.qgs',
-        layers: 'BingImage,sh_buildings,yuli_points',
-        format: 'image/png',
-        transparent: true,
-        BBOX: bbox,
-        maxZoom: 22,
-        version: '1.3.0',
-        crs: L.CRS.EPSG4326,
-        SRS: 'EPSG:4326',
-        REQUEST: 'GetMap',
-        WIDTH: '600',
-        HEIGHT: '400',
-        updateInterval: 1000, //在平移时，瓦片将会在 updateInterval 毫秒内最多只更新一次。
-        keepBuffer: 10, //平移地图时，在卸载之前保留这么多行和列的图块。
-        cache: true,
-      })
-      .addTo(map)
-
-    // 绑定地图视图变化事件，当视图发生变化时更新动态瓦片图层
-    // map.on('moveend', () => {
-    //   const bounds = map.getBounds()
-    //   const ne = bounds.getNorthEast()
-    //   const sw = bounds.getSouthWest()
-    //   const bbox = `${sw.lat},${sw.lng},${ne.lat},${ne.lng}`
-    //   wmsLayer.setParams({ bbox })
-    // })
-
-    // map.on('moveend', L.Util.THROTTLE(function() {
-    // 	const bounds = map.getBounds();
-    // 	const ne = bounds.getNorthEast();
-    // 	const sw = bounds.getSouthWest();
-    // 	const bbox = `${sw.lat},${sw.lng},${ne.lat},${ne.lng}`;
-    // 	wmsLayer.setParams({ bbox });
-    // }, 100));
-
-    // 添加缩放控件
-    L.control
-      .zoom({
-        position: 'topright',
-      })
-      .addTo(map)
-  }
+  let map = null
 
   onMounted(() => {
-    initMap()
+    AMapLoader.load({
+      key: 'e1c72e6ab96ade5f09406d4583b53c57', // 申请好的Web端开发者Key，首次调用 load 时必填
+      version: '2.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+      plugins: ['AMap.Scale'], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+    })
+      .then((AMap) => {
+        map = new AMap.Map('container', {
+          // 设置地图容器id
+          viewMode: '3D', // 是否为3D地图模式
+          zoom: 11, // 初始化地图级别
+          center: [116.397428, 39.90923], // 初始化地图中心点位置
+        })
+
+        // 添加CSS来隐藏logo
+        const style = document.createElement('style')
+        style.innerHTML = `
+	      .amap-logo {
+	        display: none !important;
+	      }
+		  .amap-copyright {
+			  display: none !important;
+		  }
+	    `
+        document.head.appendChild(style)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  })
+
+  onUnmounted(() => {
+    map?.destroy()
   })
 </script>
 
-<style>
-  /* 必要的Leaflet CSS重置 */
-  .leaflet-container {
-    height: 400px;
+<template>
+  <div id="container"></div>
+</template>
+
+<style scoped>
+  #container {
     width: 100%;
-    max-width: 100%;
-    max-height: 100%;
+    height: 800px;
   }
 </style>
