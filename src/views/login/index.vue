524122c4c67b1ee1799b7d0f7bb7031143ea09a9
MYC_Desktop <shuboeiw@gmail.com>
2024-04-20 22:19:22 +0800

    删除二维码登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,16 +6,19 @@
         <img src="@/assets/image/login/side-logo.png" />
       </div>
       <div class="login-form">
-        <!-- <div class="info-qrcode">{{ accountLogin ? '扫码登录' : '账号登录' }}</div>
-        <img src="@/assets/image/login/qrcode-icon.png" class="qrcode" @click="handleClick" /> -->
-        <el-button-group class="button-group">
+        <div class="info-qrcode">{{ accountLogin ? '短信登录' : '账号登录' }}</div>
+        <img src="@/assets/image/login/qrcode-icon.png" class="qrcode" @click="handleClick" />
+        <!-- <el-button-group class="button-group">
           <el-button type="info" @click="switchForm('LoginForm')">账号登录</el-button>
           <el-button type="info" @click="switchForm('LoginQrcode')">二维码登录</el-button>
           <el-button type="info" @click="switchForm('SMSLoginForm')">短信登录</el-button>
-        </el-button-group>
+        </el-button-group> -->
 
-        <LoginForm v-if="currentForm === 'LoginForm'" />
+        <!-- <LoginForm v-if="currentForm === 'LoginForm'" />
         <LoginQrcode v-else-if="currentForm === 'LoginQrcode'" />
+        <SMSLoginForm v-else /> -->
+
+        <LoginForm v-if="accountLogin" />
         <SMSLoginForm v-else />
       </div>
     </div>
@@ -29,10 +32,17 @@
   import LoginQrcode from './components/LoginQrcode.vue'
   import SMSLoginForm from './components/SMSLoginForm.vue'
 
-  const currentForm = ref('LoginForm')
-  const switchForm = (formName) => {
-    currentForm.value = formName
+  const accountLogin = ref<boolean>(true)
+
+  const handleClick = () => {
+    console.log('=======', accountLogin)
+    accountLogin.value = !accountLogin.value
   }
+
+  // const currentForm = ref('LoginForm')
+  // const switchForm = (formName) => {
+  //   currentForm.value = formName
+  // }
 </script>
 <style lang="scss" scoped>
   @import './index';
