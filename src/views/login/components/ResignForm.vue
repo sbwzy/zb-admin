074860c9e1e67f8e0d7aefe04df1aae1734254bb
MYC_Desktop <shuboeiw@gmail.com>
2024-04-24 22:53:17 +0800

    添加链接跳转到注册页面

--- src/views/login/components/ResignForm.vue
+++ src/views/login/components/ResignForm.vue
@@ -4,24 +4,24 @@
     <h2 class="title">公房监测管理平台</h2>
   </div>
   <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules">
-    <el-form-item label="" prop="phone">
+    <el-form-item label="" prop="username">
       <el-input
-        v-model="ruleForm.phone"
-        placeholder="请输入手机号"
+        v-model="ruleForm.username"
+        placeholder="请输入用户名"
         auto-complete="on"
         style="position: relative"
         @keyup.enter="submitForm(ruleFormRef)"
       >
-        <!-- <template #prefix>
+        <template #prefix>
           <el-icon class="el-input__icon"><UserFilled /></el-icon>
-        </template> -->
+        </template>
       </el-input>
     </el-form-item>
 
-    <el-form-item label="" prop="yzm">
-      <!-- <el-input
-        v-model="ruleForm.yzm"
-        placeholder="请输入验证码"
+    <el-form-item label="" prop="password">
+      <el-input
+        v-model="ruleForm.password"
+        placeholder="请输入密码"
         auto-complete="on"
         :type="passwordType"
         @keyup.enter="submitForm(ruleFormRef)"
@@ -34,73 +34,59 @@
             <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
           </div>
         </template>
-      </el-input> -->
-      <el-row :gutter="0" style="width: 100%">
-        <el-col :span="14">
-          <el-input v-model="ruleForm.yzm" placeholder="请输入验证码" style="position: relative"></el-input>
-        </el-col>
-        <el-col :span="10" class="button-col">
-          <el-button type="primary" :disabled="countdown > 0" @click="sendCode">
-            发送验证码{{ countdown > 0 ? `(${countdown}s)` : '' }}
-          </el-button>
-        </el-col>
-      </el-row>
+      </el-input>
     </el-form-item>
 
     <el-form-item style="width: 100%">
       <el-button :loading="loading" class="login-btn" type="primary" @click="submitForm(ruleFormRef)">登录</el-button>
     </el-form-item>
-    <el-link class="register" type="primary" @click="onRegister">注册账号</el-link>
+    <el-link class="register" type="primary" @click="handleRegister">注册账号</el-link>
   </el-form>
 </template>
-
 <script lang="ts" setup>
-  import { ref, reactive, onUnmounted, defineProps } from 'vue'
+  import { ref, reactive } from 'vue'
   import type { FormInstance } from 'element-plus'
   import { ElNotification } from 'element-plus'
   import { useRouter } from 'vue-router'
   import { useUserStore } from '@/store/modules/user'
   import { getTimeStateStr } from '@/utils/index'
-  import { login } from '@/api/user'
 
   const router = useRouter()
   const UserStore = useUserStore()
   const ruleFormRef = ref<FormInstance>()
-  //const passwordType = ref('password')
+  const passwordType = ref('password')
   const loading = ref(false)
 
   const rules = reactive({
-    phone: [
-      { required: true, message: '请输入手机号', trigger: 'blur' },
-      { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确', trigger: 'blur' },
-    ],
-    yzm: [{ required: true, message: '请输入验证码', trigger: 'blur' }],
+    password: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+    username: [{ required: true, message: '请输入密码', trigger: 'blur' }],
   })
 
   // 表单数据
   const ruleForm = reactive({
-    phone: '',
-    yzm: '',
+    username: 'Resign',
+    password: '123456',
   })
+
+  // 显示密码图标
+  const showPwd = () => {
+    passwordType.value = passwordType.value === 'password' ? '' : 'password'
+  }
 
   const submitForm = (formEl: FormInstance | undefined) => {
     if (!formEl) return
     formEl.validate((valid) => {
       if (valid) {
         loading.value = true
-        login(ruleForm).then((res) => {
-          console.log('打印', res)
-          //把后面代码写在这里面
-        })
         // 登录
         setTimeout(async () => {
-          await UserStore.login1(ruleForm)
+          await UserStore.login(ruleForm)
           await router.push({
             path: '/',
           })
           ElNotification({
             title: getTimeStateStr(),
-            message: '欢迎登录 监测管理平台',
+            message: '欢迎登录 公房监测管理平台',
             type: 'success',
             duration: 3000,
           })
@@ -113,36 +99,9 @@
     })
   }
 
-  const countdown = ref(0)
-  let intervalId = null
-
-  const sendCode = () => {
-    if (countdown.value > 0) {
-      return
-    }
-    countdown.value = 60
-    intervalId = setInterval(() => {
-      countdown.value--
-      if (countdown.value === 0) {
-        clearInterval(intervalId)
-      }
-    }, 1000)
-
-    onUnmounted(() => {
-      if (intervalId) {
-        clearInterval(intervalId)
-      }
-    })
-
-    return {
-      countdown,
-      sendCode,
-    }
+  const handleRegister = () => {
+    // 在这里处理点击事件
   }
-
-  const props = defineProps({
-    onRegister: Function,
-  })
 </script>
 
 <style lang="scss" scoped>
